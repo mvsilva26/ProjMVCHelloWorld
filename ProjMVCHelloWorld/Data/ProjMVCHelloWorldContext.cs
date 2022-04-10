30a02ab48a4b047b84f9c2e717dd59789fe25248
Michael Silva <michael.steam@outlook.com>
2022-04-10 11:47:57 -0300

    Adicionar arquivos de projeto.

---
+++ ProjMVCHelloWorld/Data/ProjMVCHelloWorldContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjMVCHelloWorld.Models;
+
+namespace ProjMVCHelloWorld.Data
+{
+    public class ProjMVCHelloWorldContext : DbContext
+    {
+        public ProjMVCHelloWorldContext (DbContextOptions<ProjMVCHelloWorldContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjMVCHelloWorld.Models.Projects> Projects { get; set; }
+    }
+}
---
+++ ProjMVCHelloWorld/Models/Projects.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjMVCHelloWorld.Models
+{
+    public class Projects
+    {
+
+
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        #endregion
+
+
+
+
+    }
+}
